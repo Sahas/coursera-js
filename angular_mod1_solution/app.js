72c4e2a0f3a5fee47e2f93f7b8f64afdc0a5ba70
sahas <sahas052@gmail.com>
2017-04-20 23:48:48 +0530

    First module complete

---
+++ angular_mod1_solution/app.js
@@ -0,0 +1,42 @@
+(function(){
+
+  'use strict';
+  angular.module('FoodCheckApp', []).controller('MainController', MainController);
+
+
+  MainController.$inject = ['$scope'];
+  function MainController($scope){
+
+     $scope.onkeyup = function(){
+      if($scope.items !== ''){
+          $scope.message= '';
+      }
+    }
+
+
+    $scope.check = function(){
+
+      if($scope.items === ''){
+        $scope.message= 'Please enter data first';
+        $scope.messageColor = 'text-warning';
+      }
+      else{
+        var itemList = $scope.items.split(',');
+        var filteredItemList = itemList.filter(function(item){
+          if(item.trim()!=''){
+            return item;
+          }
+
+        });
+        if(filteredItemList.length > 3){
+          $scope.message = 'Too much';
+        }
+        else{
+          $scope.message = 'Enjoy!'
+        }
+        $scope.messageColor = 'text-success';
+      }
+
+    }
+  }
+})();
